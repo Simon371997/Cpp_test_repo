9fda049a63eb0cb0c2e08f139786067f628eb32d
Simon Henes <simon.henes@live.de>
2023-08-08 13:44:49 +0200

    test

--- test.cpp
+++ test.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
+
+double power(double base, int exponent)
+{
+     double result = 1;
+     for(int i = 0; i < exponent; i++)
+     {
+          result = result * base;
+     }
+     return result;
+}
+
+void print_pow(double base, int exponent)
+{
+   
+     double mypower = power(base, exponent);
+     cout << base << " raise to the " << exponent << " power is " <<mypower << endl;
+}
+
 
 int main() 
 {
-   
-     int slices;
+     //This is a Comment
+     double base;
+     int exponent;
+     cout<< "What is the base? ";
+     cin >> base;
+     cout << "What is the exponent? ";
+     cin >> exponent;
+     double mypower = power(base, exponent);
+     cout << mypower << endl;
+     print_pow(base, exponent);
 
-     cout << "How manny slices of Pizza you have?";
-     cin >> slices;
-
-     cout << "You have " <<  slices << " slices of Pizza" <<endl;
- 
 }
 
